5b83053d37ef83a398616673fd0667d4129d5f43
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2024-01-17 20:05:48 +1100

    feat: hello world

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,6 @@ import { type AppType } from "next/app";
 
 import { api } from "@/utils/api";
 
-import "@/styles/globals.css";
-
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
