75ffe9529a6ec4306bef7304b8e9919bdfb1d63d
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2024-01-29 20:14:43 +1100

    feat: add first part of shadcn UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Head from "next/head";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="text-red-500">Hello World</div>
+      <Button>Some Button</Button>
     </>
   );
 }
